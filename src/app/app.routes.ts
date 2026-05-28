dc39f69e772e85ee5c7caafc31f3cfd6bf5facb4
The-RuBe <rubenroldanjimenez12@gmail.com>
2026-05-28 10:18:02 +0200

    Add header, footer, home & soporte components
    Introduce new UI components (header, footer, home, soporte) with templates, styles and basic unit tests; add them to the root App imports. Replace the default placeholder app.html with app-header, app-router-outlet and app-footer. Update routing to register Home and Soporte routes and add a LoL.ico asset. Also add an analytics ID to angular.json.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Soporte } from '../components/soporte/soporte';
+import { Home } from '../components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: ' ', component: Home},
+    {path: '/soporte', component: Soporte}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../components/header/header';
+import { Footer } from '../components/footer/footer';
+import { Home } from '../components/home/home';
+import { Soporte } from '../components/soporte/soporte';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Home, Soporte],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
