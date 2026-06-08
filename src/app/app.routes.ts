f99b95c815cf7dbb6a7fd4a541618bae70a70344
The-RuBe <rubenroldanjimenez12@gmail.com>
2026-06-08 10:07:13 +0200

    Arreglos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Soporte } from '../components/soporte/soporte';
 import { Home } from '../components/home/home';
+import { CharacterMultiplerole } from '../components/character-multiplerole/character-multiplerole';
 
 export const routes: Routes = [
     {path: '', component: Home},
-    {path: 'soporte', component: Soporte}
+    {path: 'soporte', component: Soporte},
+    {path: 'character/multiplerole', component: CharacterMultiplerole}
 ];
